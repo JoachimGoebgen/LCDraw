1dc957fe0ccb62c96d9d0aa8175531006c31940b
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-12-13 21:41:50 +0100

    [GUI] first working demo

--- LCDraw-learn/LCDraw-learn.py
+++ LCDraw-learn/LCDraw-learn.py
@@ -1,3 +1,4 @@
+import os
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -6,9 +7,9 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.slider import Slider
 from kivy.uix.label import Label
 from kivy.graphics import Color, Line, Rectangle
+from ConfigParser import SafeConfigParser
+from kivy.core.window import Window
 
-class MainBoxLayout(BoxLayout):
-    pass
 
 class PaintWidget(Widget):
     
@@ -24,19 +25,24 @@ class PaintWidget(Widget):
 
     def on_touch_up(self, touch):
         if self.collide_point(touch.x, touch.y):
-            self.export_to_png("test.bmp")
+            self.export_to_png(".processing.bmp") #dirty but working
         self.canvas.clear()
+
+class LCDrawApp(App):
+    def build(self):
+        cfg_full_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
+        configParser = SafeConfigParser()
+        configParser.read(cfg_full_path)
+        print(configParser.get('base', 'windowWidth'))
+        Window.size = (int(configParser.get('base', 'windowWidth')), int(configParser.get('base', 'windowHeight')))
+        return MainBoxLayout()
+        
+class MainBoxLayout(BoxLayout):
+    pass
 
 class InfoBoxLayout(BoxLayout):
     pass
     
-class ValueBoxLayout(BoxLayout):
-    pass
-    
-class LCDrawApp(App):
-    def build(self):
-        return MainBoxLayout()
-        
 if __name__ == '__main__':
     LCDrawApp().run()
     
