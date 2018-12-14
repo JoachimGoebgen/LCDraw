0a2299bc93f92d3dd59b342abd3f8b80be5cfbf9
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-12-14 01:32:09 +0100

    [GUI] import and show labels from config.ini

--- LCDraw-label/LCDraw-label.py
+++ LCDraw-label/LCDraw-label.py
@@ -7,11 +7,21 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.slider import Slider
 from kivy.uix.label import Label
 from kivy.graphics import Color, Line, Rectangle
-from ConfigParser import SafeConfigParser
+from configparser import SafeConfigParser
 from kivy.core.window import Window
 
 
+cfg_full_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
+configParser = SafeConfigParser()
+configParser.read(cfg_full_path)
+
+class LCDrawApp(App):
+    def build(self):
+        Window.size = (int(configParser.get('base', 'windowWidth')), int(configParser.get('base', 'windowHeight')))
+        return MainBox()
+   
 class PaintWidget(Widget):
+    processing_full_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.processing.png'))
     
     def on_touch_down(self, touch):
         if self.collide_point(touch.x, touch.y):
@@ -25,24 +35,24 @@ class PaintWidget(Widget):
 
     def on_touch_up(self, touch):
         if self.collide_point(touch.x, touch.y):
-            self.export_to_png(".processing.bmp") #dirty but working
+            self.export_to_png(self.processing_full_path) #dirty but working
+            self.parent.ids.dataBox.switch_label()
         self.canvas.clear()
+     
+class DataBox(BoxLayout):
+    labelIndex = 0
+    labels = configParser.get('base', 'labels')
 
-class LCDrawApp(App):
-    def build(self):
-        cfg_full_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
-        configParser = SafeConfigParser()
-        configParser.read(cfg_full_path)
-        print(configParser.get('base', 'windowWidth'))
-        Window.size = (int(configParser.get('base', 'windowWidth')), int(configParser.get('base', 'windowHeight')))
-        return MainBoxLayout()
-        
-class MainBoxLayout(BoxLayout):
-    pass
-
-class InfoBoxLayout(BoxLayout):
-    pass
+    def switch_label(self):
+        self.labelIndex = (self.labelIndex + 1) % len(self.labels)
+        self.parent.ids.labelLabel.text = self.labels[self.labelIndex]
     
+class MainBox(BoxLayout):
+    pass
+
 if __name__ == '__main__':
     LCDrawApp().run()
+
+
+
     
