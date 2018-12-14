3a5a605b9887d1ac84375456ce6a40e01e761358
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-12-14 17:37:10 +0100

    [App] add redo-option and lots of restructuring

--- LCDraw-label/LCDraw-label.py
+++ LCDraw-label/LCDraw-label.py
@@ -11,6 +11,8 @@ from kivy.uix.label import Label
 from kivy.graphics import Color, Line, Rectangle
 from kivy.core.window import Window
 
+labelToCountDict = {}
+
 temp_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.img.png'))
 conf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
 conf = ConfigParser()
@@ -19,6 +21,7 @@ conf.read(conf_path)
 windowWidth = int(conf.get('base', 'windowWidth'))
 windowHeight = int(conf.get('base', 'windowHeight'))
 reSize_px = int(conf.get('base', 'imgSize_px'))
+
 
 class LCDrawApp(App):
     def build(self):
@@ -40,19 +43,38 @@ class PaintWidget(Widget):
 
     def on_touch_up(self, touch):
         if self.collide_point(touch.x, touch.y):
-            self.saveImage()
-            self.parent.ids.dataBox.switch_label()
+            self.export_to_png(temp_path) # if its stupid and it works, it aint stupid
+            App.get_running_app().root.process_image()
         self.canvas.clear()
 
-    def saveImage(self):
-        self.export_to_png(temp_path) # if its stupid and it works, it aint stupid
+
+class DataBox(BoxLayout):
+    dataBoxSize_ratio = 1 - float(windowHeight) / windowWidth
+
+
+class MainBox(BoxLayout):
+    labelIndex = 0
+    labels = conf.get('base', 'labels')
+    global labelToCountDict
+    for char in labels:
+        labelToCountDict[char] = 0
+
+    def process_image(self):
         img = numpy.asarray(imageio.imread(temp_path))
         (u_min, u_mid, u_max, v_min, v_mid, v_max, size_half) = self.getSymbolBounds(img)
         if (u_min >= u_max or v_min >= v_max):
             return
-        img_cut = img[v_mid-size_half:v_mid+size_half, u_mid-size_half:u_mid+size_half, 0]
-        img_resized = resize(img_cut, (reSize_px, reSize_px), anti_aliasing=False)
+        img_boundingBox = img[v_mid-size_half:v_mid+size_half, u_mid-size_half:u_mid+size_half, 0]
+        img_resized = resize(img_boundingBox, (reSize_px, reSize_px), anti_aliasing=False)
         imageio.imwrite('.img_resized.png', img_resized[:, :])
+        self.update_labels(1)
+
+    def rollback_image(self):
+        self.update_labels(-1)
+
+    def update_labels(self, sign):
+        self.labelIndex = (self.labelIndex + sign) % len(self.labels)
+        self.ids.labelLabel.text = self.labels[self.labelIndex]    
 
     def getSymbolBounds(self, img):
         (u_min, u_max, v_min, v_max) = (img.shape[1], 0, img.shape[0], 0)
@@ -69,19 +91,6 @@ class PaintWidget(Widget):
                         u_max = u                
         return (u_min, (u_max + u_min) / 2, u_max, v_min, (v_max + v_min) / 2, v_max, max(u_max - u_min, v_max - v_min) / 2)
     
-
-
-class DataBox(BoxLayout):
-    dataBoxSize_ratio = 1 - float(windowHeight) / windowWidth
-    labelIndex = 0
-    labels = conf.get('base', 'labels')
-    
-    def switch_label(self):
-        self.labelIndex = (self.labelIndex + 1) % len(self.labels)
-        self.parent.ids.labelLabel.text = self.labels[self.labelIndex]
-    
-class MainBox(BoxLayout):
-    pass
 
 if __name__ == '__main__':
     LCDrawApp().run()
